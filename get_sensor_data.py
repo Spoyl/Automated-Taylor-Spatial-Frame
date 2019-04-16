cc575b115ca0536fc92698a43f1033b8114f1d73
Oliver Stocks <stocksolly@gmail.com>
2019-04-16 13:35:02 +0100

    functionize get_sensor_data.py

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -9,42 +9,72 @@ import csv
 import serial
 import time
  
-#create filename
-name="test_dat"
+FILENAME="test_dat"
+PORT="com10"
+BAUD_RATE=9600
+    
+    
+def writeData2File(f,writer):
+    """
+    Reads a line of serial data (x, y, z) using access_serial_data and
+    writes the data to a csv file.
+    
+        Parameters
+        ----------
+        
+            f: (str) pointer to open file set to 'wt'
+            
+            writer: (str) pointer to a csv writer object
+    """
+    
+    writer.writerow(['[Header]'])
+    date=time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    writer.writerow(['date='+date])
+    writer.writerow(['Column1=X axis'])
+    writer.writerow(['Column2=Y axis'])
+    writer.writerow(['Column3=Z axis'])
+    writer.writerow(['[Data]'])
+    
+    writer.writerow((x,y,z))
 
-f=open(name+'.csv', 'wt')
 
-#Initialize the CSV writer
-writer = csv.writer(f,delimiter='\t')
-
-#Add file header
-writer.writerow(['[Header]'])
-date=time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-writer.writerow(['date='+date])
-writer.writerow(['Column1=X axis'])
-writer.writerow(['Column2=Y axis'])
-writer.writerow(['Column3=Z axis'])
-writer.writerow(['[Data]'])
- 
-#Open Serial port
-ser = serial.Serial('com10',9600,timeout=1)
-
-#read data
-for t in range(100):
+def access_serial_data(baud_rate,port="com10"):
+    """
+    Reads data coming through the serial port assuming a three int format
+    as defined in the corresponding arduino program.
+    
+    
+        Parameters
+        ----------
+                    
+            baud_rate: (int) Defines the baud rate of the port being read.
+                    The rate can be found in the Arduino code - line should 
+                    read "Serial.begin(<baud_rate>)".
+                    
+            port: (str) Define the com port to be read. Defaults to com10. 
+                        The port can be found under "tools - port" in the 
+                        Arduino interface.
+                    
+        Returns
+        -------
+        
+            [x, y, z]: (array) Holds the value for each axis
+    """
     
     kk=ser.readline()
-    
-    #String in form <xval>\t<yval>\t<zval>\n
     kk2=kk.split()
     print(kk2[0], kk2[1], kk[2])
     x=kk2[0]
     y=kk2[1]
     z=kk2[2]
+        
+    return [x,y,z]
     
-    #We put each data point in a new row of the data file.
-    writer.writerow((x,y,z))
 
- 
-#When finish, close the file and the serial port.
-f.close()
-ser.close()
+if __name__ =="__main__":
+    f=open(FILENAME+'.csv', 'wt')
+    writer = csv.writer(f,delimiter='\t')
+    writeData2File(f, writer)
+    #ser = serial.Serial(PORT,BAUD_RATE,timeout=1)
+    f.close()
+    ser.close()
