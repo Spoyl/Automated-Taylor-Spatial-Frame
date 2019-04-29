ff862b38e939c2d5595660cd21b06abcfbf2026f
Oliver Stocks <stocksolly@gmail.com>
2019-04-29 20:47:15 +0100

    plot average powers

--- process_data.py
+++ process_data.py
@@ -6,39 +6,63 @@ Created on Mon Apr 29 15:41:12 2019
 """
 
 from readFile import *
+from os import listdir
+from numpy import mean
 
 BASE_DIR="C:\\Users\\Oliver\\Desktop\\GDPTestData\\"
 FILENAME="TestData2019_04_25_19_25_39"
-FILE_DIR=BASE_DIR+FILENAME
 FC1 = 300
 FC2 = 350
 FS = 800
+Z_AVE_ARRAY = []
+X_AVE_ARRAY = []
+Y_AVE_ARRAY = []
 
-X_ARRAY, Y_ARRAY, Z_ARRAY=readDataFile(FILE_DIR)
-X_ARRAY,Y_ARRAY,Z_ARRAY = rm_nans(X_ARRAY,Y_ARRAY,Z_ARRAY)
-x_mean,y_mean,z_mean = find_means(X_ARRAY, Y_ARRAY, Z_ARRAY)
-
-X_ARRAY,Y_ARRAY,Z_ARRAY = rm_means(X_ARRAY,Y_ARRAY,Z_ARRAY,x_mean,y_mean,z_mean)
-
-b, a = butter_band(FC1,FC2,FS)
-X_ARRAY_FILT,Y_ARRAY_FILT,Z_ARRAY_FILT = apply_filter(b,a,X_ARRAY,
-                                                      Y_ARRAY,Z_ARRAY)
-
-plot_signal_trio(X_ARRAY_FILT,Y_ARRAY_FILT,Z_ARRAY_FILT, FS)
-
-# CALCULATE POWER SPECTRA
-PSD_ARRAY = calc_power_spectra(X_ARRAY_FILT,Y_ARRAY_FILT,Z_ARRAY_FILT, FS)
-
+for file_name in listdir(BASE_DIR):
+    
+    FILE_DIR=BASE_DIR+file_name
+    
+    # GET ZYX DATA
+    X_ARRAY, Y_ARRAY, Z_ARRAY=readDataFile(FILE_DIR)
+    X_ARRAY,Y_ARRAY,Z_ARRAY = rm_nans(X_ARRAY,Y_ARRAY,Z_ARRAY)
+    x_mean,y_mean,z_mean = find_means(X_ARRAY, Y_ARRAY, Z_ARRAY)
+    X_ARRAY,Y_ARRAY,Z_ARRAY = rm_means(X_ARRAY,Y_ARRAY,Z_ARRAY,x_mean,y_mean,z_mean)
+    # ------------
+    
+    # FILTER THE DATA
+    b, a = butter_band(FC1,FC2,FS)
+    X_ARRAY_FILT,Y_ARRAY_FILT,Z_ARRAY_FILT = apply_filter(b,a,X_ARRAY,
+                                                          Y_ARRAY,Z_ARRAY)
+    
+    # CALCULATE POWER SPECTRA
+    PSD_ARRAY = calc_power_spectra(X_ARRAY_FILT,Y_ARRAY_FILT,Z_ARRAY_FILT, FS)
+    
+    # PLOTS
+    #plot_signal_trio(X_ARRAY_FILT,Y_ARRAY_FILT,Z_ARRAY_FILT, FS)
+    #plot_power_spectra(PSD_ARRAY)
+    
+    X_AVE_ARRAY.append(mean(PSD_ARRAY[1]))
+    Y_AVE_ARRAY.append(mean(PSD_ARRAY[3]))
+    Z_AVE_ARRAY.append(mean(PSD_ARRAY[5]))
+    
 plt.figure(figsize=(16,6))
-plt.title("Power Spectral Density")
-plt.plot(PSD_ARRAY[0],PSD_ARRAY[1])
-plt.plot(PSD_ARRAY[2],PSD_ARRAY[3])
-plt.plot(PSD_ARRAY[4],PSD_ARRAY[5])
-plt.ylabel("Power")
-plt.xlabel("Frequency, Hz")
+t_ax = np.arange(0,len(X_AVE_ARRAY))
+plt.plot(t_ax, X_AVE_ARRAY)
 plt.grid()
 plt.show()
-# -----------------------
+
+plt.figure(figsize=(16,6))
+t_ax = np.arange(0,len(Y_AVE_ARRAY))
+plt.plot(t_ax, Y_AVE_ARRAY)
+plt.grid()
+plt.show()
+
+plt.figure(figsize=(16,6))
+t_ax = np.arange(0,len(Z_AVE_ARRAY))
+plt.plot(t_ax, Z_AVE_ARRAY)
+plt.grid()
+plt.show()
+
 
 #x_fft=np.fft.fft(X_ARRAY)
 #f=np.fft.fftfreq(len(X_ARRAY), 1/800)
--- readFile.py
+++ readFile.py
@@ -15,15 +15,6 @@ import matplotlib.pyplot as plt
 from scipy import signal
 import numpy as np
 
-## GLOBAL VARIABLES
-#BASE_DIR="C:\\Users\\Oliver\\Desktop\\GDPTestData\\"
-#FILENAME="TestData2019_04_25_19_10_36"
-#FILE_DIR=BASE_DIR+FILENAME
-#FC1 = 300
-#FC2 = 350
-#FS = 800
-## ----------------
-
 
 def plot_signal_trio(X_ARRAY,Y_ARRAY,Z_ARRAY, FS):
     """
@@ -142,7 +133,7 @@ def readDataFile(FILE_DIR):
     Y_ARRAY = []
     Z_ARRAY = []
     
-    f = open(FILE_DIR+'.txt', "r")
+    f = open(FILE_DIR, "r")
     lines = f.readlines()
     lines = lines[6:]
     
@@ -186,39 +177,16 @@ def calc_power_spectra(X_ARRAY, Y_ARRAY, Z_ARRAY, FS):
     return PSD_ARRAY
 
 
-#X_ARRAY, Y_ARRAY, Z_ARRAY=readDataFile(FILE_DIR)
-#X_ARRAY,Y_ARRAY,Z_ARRAY = rm_nans(X_ARRAY,Y_ARRAY,Z_ARRAY)
-#x_mean,y_mean,z_mean = find_means(X_ARRAY, Y_ARRAY, Z_ARRAY)
-#
-#t=np.arange(0,len(X_ARRAY))/FS # Create time axis
-#
-#X_ARRAY,Y_ARRAY,Z_ARRAY = rm_means(X_ARRAY,Y_ARRAY,Z_ARRAY)
-#
-#b, a = butter_band(FC1,FC2,FS)
-#X_ARRAY_FILT,Y_ARRAY_FILT,Z_ARRAY_FILT = apply_filter(b,a,X_ARRAY,
-#                                                      Y_ARRAY,Z_ARRAY)
-#
-#plot_signal_trio(X_ARRAY_FILT,Y_ARRAY_FILT,Z_ARRAY_FILT)
-#
-## CALCULATE POWER SPECTRA
-#PSD_ARRAY = calc_power_spectra(X_ARRAY_FILT,Y_ARRAY_FILT,Z_ARRAY_FILT)
-#
-#plt.figure(figsize=(16,6))
-#plt.title("Power Spectral Density")
-#plt.plot(PSD_ARRAY[0],PSD_ARRAY[1])
-#plt.plot(PSD_ARRAY[2],PSD_ARRAY[3])
-#plt.plot(PSD_ARRAY[4],PSD_ARRAY[5])
-#plt.ylabel("Power")
-#plt.xlabel("Frequency, Hz")
-#plt.grid()
-#plt.show()
-## -----------------------
-#
-##x_fft=np.fft.fft(X_ARRAY)
-##f=np.fft.fftfreq(len(X_ARRAY), 1/800)
-##plt.figure(figsize=(16,6))
-##plt.title("X axis Fft")
-##plt.plot(f, x_fft)
-##plt.xlabel("Frequency, Hz")
-##plt.grid()
-##plt.show()        
+def plot_power_spectra(PSD_ARRAY):
+    
+    plt.figure(figsize=(16,6))
+    plt.plot(PSD_ARRAY[0],PSD_ARRAY[1],label="x axis")
+    plt.plot(PSD_ARRAY[2],PSD_ARRAY[3],label="y axis")
+    plt.plot(PSD_ARRAY[4],PSD_ARRAY[5],label="z axis")
+    plt.title("Power Spectral Density")
+    plt.ylabel("Power")
+    plt.xlabel("Frequency, Hz")
+    plt.legend()
+    plt.grid()
+    plt.show()
+      
